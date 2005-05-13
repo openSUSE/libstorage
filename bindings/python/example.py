c333c85090b487f0901cab902676aed6392fd069
Arvin Schnell <aschnell@suse.de>
2005-05-13 07:50:16 +0000

    - added enum usage example

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -2,7 +2,9 @@
 
 import LibStorage
 
+
 c = LibStorage.createStorageInterface (1, 0, 1)
+
 
 disks = LibStorage.dequestring ()
 c.getDisks (disks)
@@ -11,11 +13,17 @@ for disk in disks:
 
     print disk
 
-    partitioninfos = LibStorage.dequepartitioninfo()
+    partitioninfos = LibStorage.dequepartitioninfo ()
     c.getPartitionsOfDisk (disk, partitioninfos)
 
     for partitioninfo in partitioninfos:
 
         print partitioninfo.name,partitioninfo.cylStart,partitioninfo.cylSize
+
+
+fscapabilities = LibStorage.FsCapabilities ();
+c.getFsCapabilities (LibStorage.REISERFS, fscapabilities);
+print fscapabilities.isExtendable,fscapabilities.minimalFsSizeK
+
 
 LibStorage.destroyStorageInterface (c)
