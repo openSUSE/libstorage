b5931645afda12e1b2c1108285b0c7a2b6e012d5
Arvin Schnell <aschnell@suse.de>
2005-06-14 09:54:16 +0000

    - added testsuite for real testing on disk

---
+++ testsuite-real/lvmcreate1.cc
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+#include <y2storage/StorageInterface.h>
+
+using namespace storage;
+using namespace std;
+
+
+void
+doit ()
+{
+    StorageInterface* s = createDefaultStorageInterface ();
+
+
+    cout << s->destroyPartitionTable ("hdb", s->defaultDiskLabel ()) << '\n';
+
+
+    deque <string> pds;
+    pds.push_back ("hdb");
+    cout << s->createLvmVg ("trash", 4, false, pds) << '\n';
+
+
+    string device;
+    cout << s->createLvmLv ("trash", "junk", 500, 1, device) << '\n';
+
+
+    cout << s->changeFormatVolume (device, true, EXT3) << '\n';
+    cout << s->changeMountPoint (device, "/mnt") << '\n';
+
+
+    cout << s->commit () << '\n';
+
+
+    delete s;
+}
+
+
+int
+main (int argc, char** argv)
+{
+    initDefaultLogger ();
+
+    cout << "first run\n";
+    doit ();
+
+    cout << "second run\n";
+    doit ();
+
+    exit (EXIT_SUCCESS);
+}
---
+++ testsuite-real/lvmcreate2.cc
@@ -0,0 +1,56 @@
+
+#include <iostream>
+
+#include <y2storage/StorageInterface.h>
+
+using namespace storage;
+using namespace std;
+
+
+void
+doit ()
+{
+    StorageInterface* s = createDefaultStorageInterface ();
+
+
+    cout << s->destroyPartitionTable ("hdb", s->defaultDiskLabel ()) << '\n';
+
+
+    string name = "hdb1";
+    cout << s->createPartitionKb ("hdb", PRIMARY, 0, 1000000, name) << '\n';
+    cout << name << '\n';
+
+
+    deque <string> pds;
+    pds.push_back (name);
+    cout << s->createLvmVg ("trash", 4, false, pds) << '\n';
+
+
+    string device;
+    cout << s->createLvmLv ("trash", "junk", 500, 1, device) << '\n';
+
+
+    cout << s->changeFormatVolume (device, true, EXT3) << '\n';
+    cout << s->changeMountPoint (device, "/mnt") << '\n';
+
+
+    cout << s->commit () << '\n';
+
+
+    delete s;
+}
+
+
+int
+main (int argc, char** argv)
+{
+    initDefaultLogger ();
+
+    cout << "first run\n";
+    doit ();
+
+    cout << "second run\n";
+    doit ();
+
+    exit (EXIT_SUCCESS);
+}
