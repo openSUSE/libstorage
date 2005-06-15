112ea9a6676aa060df54883831f4249703e65c07
Holger Macht <hmacht@suse.de>
2005-06-15 10:31:30 +0000

    added tests to create and extend lvm volume groups and logical volumes

---
+++ testsuite/lvm_create.cc
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <iterator>
+#include <sstream>
+
+#include <y2storage/StorageInterface.h>
+
+using namespace storage;
+using namespace std;
+
+StorageInterface *s = 0;
+
+void print_num_lvs(const string& disk, const string& vg) {
+    deque<LvmLvInfo> plist;
+    s->getLvmLvInfo( vg, plist );
+
+    cout << plist.size() << endl;
+}
+
+/* 
+ * get all partitions on disks disks which could be added to a
+ * logical volume group ( AFAIK --> PRIMARY, LOGICAL )
+ */
+deque<string> getDevs( deque<string> disks )
+{
+    StorageInterface *siface = createStorageInterface( false, true, false );
+
+    deque<string> devs;
+    deque<PartitionInfo> pInfos;
+    
+    for ( deque<string>::iterator i = disks.begin();
+	  i != disks.end(); i++)
+    {
+	cout << siface->getPartitionInfo( *i, pInfos ) << endl;
+
+	for (deque<PartitionInfo>::iterator f = pInfos.begin ();
+	     f != pInfos.end(); f++)
+	{
+	    switch (f->partitionType)
+	    {
+	    case PRIMARY:
+		devs.push_back(f->v.name);
+		break;
+	    case LOGICAL:
+		devs.push_back(f->v.name);
+		break;
+	    default:
+		break;
+	    }
+	}
+	
+    }
+    
+    delete siface;
+
+    return devs;
+}
+
+void createLvs( const string& vg, int n, deque<string>& disks )
+{
+    printf("createLvs\n");
+
+    s = createStorageInterface( false, true, false );
+
+    /* create volume group with the above disks */
+    cout << s->createLvmVg( vg, 4, false, getDevs(disks) ) << endl;
+
+    /* create n logical volumes */
+    int ret;
+    for ( int i = 0; i < n; i++ ) 
+    {
+	ostringstream name;
+	name << "volume";
+	name << i;
+
+	string dev;
+	ret = s->createLvmLv( vg, name.str(), 100, 1, dev );
+    }
+    cout << ret << endl;
+    
+
+    for ( deque<string>::iterator i = disks.begin();
+	  i != disks.end(); i++ )
+    {
+	print_num_lvs( *i, vg);
+	print_num_lvs( *i, vg);
+    }
+
+    delete s;
+}
+
+
+void createExtendedLv( const string& vg, const string& dev )
+{
+    printf("createExtendedLv\n");
+
+    s = createStorageInterface( false, true, false );
+
+    deque<string> devs;
+    devs.push_back(dev); // add extended partition
+    
+    /* create volume group with the extended partition */
+    cout << s->createLvmVg( vg, 4, false, devs ) << endl; // FAILS
+
+    delete s;
+}
+
+
+int main( int argc_iv, char** argv_ppcv )
+{
+    system ("mkdir -p tmp");
+    setenv ("YAST2_STORAGE_TDIR", "tmp", 1);
+
+    system ("rm -rf tmp/*");
+
+    deque<string> disks;
+
+    /*
+     * Check that we can create a volume group and 50 logical volumes on
+     * ide disks.
+     */
+    disks.push_back( "/dev/hda" );
+    disks.push_back( "/dev/hdb" );
+    system ("cp data/disk_hda tmp");
+    system ("cp data/disk_hdb tmp");
+    createLvs( "system", 50, disks );
+    disks.clear();
+
+    /*
+     * Check that we can create a volume group and 50 logical volumes on
+     * scsi disks.
+     */
+    disks.push_back( "/dev/sda" );
+    disks.push_back( "/dev/sdb" );
+    system ("cp data/disk_sda tmp");
+    system ("cp data/disk_sdb tmp");
+    createLvs( "system", 50, disks );
+    disks.clear();
+
+    /*
+     * Check that we can create a volume group and 100 logical volumes on
+     * ide and scsi disks.
+     */
+    disks.push_back( "/dev/sda" );
+    disks.push_back( "/dev/sdb" );
+    disks.push_back( "/dev/hda" );
+    disks.push_back( "/dev/hdb" );
+    system ("cp data/disk_sda tmp");
+    system ("cp data/disk_sdb tmp");
+    system ("cp data/disk_hda tmp");
+    system ("cp data/disk_hdb tmp");
+    createLvs( "system", 50, disks );
+    disks.clear();
+
+    /*
+     * Check that we _cannot_ create a volume group out of an extended
+     * partition
+     */
+    system ("cp data/disk_hda tmp");
+    createExtendedLv("system", "/dev/hda4");
+}
---
+++ testsuite/lvm_extend.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <iterator>
+#include <sstream>
+
+#include <y2storage/StorageInterface.h>
+
+using namespace storage;
+using namespace std;
+
+StorageInterface *s = 0;
+
+void print_num_pvs( const string& vg) {
+    deque<string> disks;
+    disks.push_back( "/dev/hda" );
+    disks.push_back( "/dev/hdb" );
+    disks.push_back( "/dev/sda" );
+    disks.push_back( "/dev/sdb" );
+
+    int count = 0;
+    for ( deque<string>::iterator i = disks.begin();
+	  i != disks.end(); i++ )
+    {
+	deque<PartitionInfo> pinfos;
+	cout << s->getPartitionInfo( *i, pinfos )<< endl;
+
+	for ( deque<PartitionInfo>::iterator p = pinfos.begin();
+	      p != pinfos.end(); p++ )
+	{
+	    if ( p->v.usedByName == vg )
+		count++;	    
+	}
+    }
+    cout << count << endl;
+}
+
+
+void extendVg( const string& vg, deque<string> devs_extend )
+{
+    printf("extendVg\n");
+
+    s = createStorageInterface( false, true, false );
+    
+    deque<string> devs;
+    devs.push_back( "/dev/hda1" );
+    /* create volume group with the above disk */
+    cout << s->createLvmVg( vg, 4, false, devs ) << endl;
+
+    print_num_pvs( vg );
+
+    /* extend it by devs_extend given devs */
+    cout << s->extendLvmVg( vg, devs_extend ) << endl;
+    
+    print_num_pvs( vg );
+
+    delete s;
+}
+
+
+int main( int argc_iv, char** argv_ppcv )
+{
+    system ("mkdir -p tmp");
+    setenv ("YAST2_STORAGE_TDIR", "tmp", 1);
+
+    system ("rm -rf tmp/*");
+
+    system ("cp data/disk_hda tmp");
+    system ("cp data/disk_hdb tmp");
+    system ("cp data/disk_sda tmp");
+    system ("cp data/disk_sdb tmp");
+
+    deque<string> devs;
+
+    /*
+     * Check that we can extend a volume group ( consisting of one PV
+     * /dev/hda1 ) by some partitions on the same ide disk
+     */
+    devs.push_back("/dev/hda2");
+    devs.push_back("/dev/hda5");
+    extendVg( "system", devs );
+    devs.clear();
+
+    /*
+     * Check that we can extend a volume group ( consisting of one PV
+     * /dev/hda1 ) by some partitions on the another ide disk
+     */
+    devs.push_back("/dev/hdb1");
+    devs.push_back("/dev/hdb2");
+    extendVg( "system", devs );
+    devs.clear();
+
+    /*
+     * Check that we can extend a volume group ( consisting of one PV
+     * /dev/hda1 ) by some partitions on the another scsi disk
+     */
+    devs.push_back("/dev/sda1");
+    devs.push_back("/dev/sdb1");
+    extendVg( "system", devs );
+    devs.clear();
+
+}
