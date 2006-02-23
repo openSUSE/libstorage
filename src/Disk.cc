bf3b92d34b3b4e3d701141fb2ef3a6774091e1c1
Uwe Gansert <ug@suse.de>
2006-02-23 15:33:42 +0000

    reverting fix for #144595

--- src/Disk.cc
+++ src/Disk.cc
@@ -1984,7 +1984,15 @@ int Disk::doCreate( Volume* v )
 		}
 	    else
 		{
-		p->updateFsData();
+                    /* FIXME:
+                       Thomas, I know you did this line on purpose
+                       but you break partitioning in some cases with that updateFsData()
+                       you wanted to fix #144595 but now the partitioning
+                       fails if hda1 was for example a reiserfs partition
+                       and is a swap partition now, then it's formated and
+                       mounted as reiserfs but to the mountpoint "swap"
+                       */
+		//p->updateFsData();
 		}
 	    }
 	if( ret==0 && p->id()!=Partition::ID_LINUX )
