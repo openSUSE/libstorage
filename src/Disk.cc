a5bf7976bdfa7f199e7afa31c01a06a70571f984
Thomas Fehr <fehr@suse.de>
2006-03-20 14:10:11 +0000

    skip setting raid type on mac disklabel, it fails (#134652)

--- src/Disk.cc
+++ src/Disk.cc
@@ -1707,7 +1707,7 @@ int Disk::doSetType( Volume* v )
 		ret = DISK_SET_TYPE_PARTED_FAILED;
 		}
 	    }
-	if( ret==0 )
+	if( ret==0 && label!="mac" )
 	    {
 	    cmd_line.str( start_cmd );
 	    cmd_line.seekp(0, ios_base::end );
