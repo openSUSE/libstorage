3ac65c093bc35900c0284dad66556818fecbab60
Thomas Fehr <fehr@suse.de>
2006-03-27 11:40:15 +0000

    parted supports now flag raid on mac disklabel (#160084)

--- src/Disk.cc
+++ src/Disk.cc
@@ -1707,7 +1707,7 @@ int Disk::doSetType( Volume* v )
 		ret = DISK_SET_TYPE_PARTED_FAILED;
 		}
 	    }
-	if( ret==0 && label!="mac" )
+	if( ret==0 )
 	    {
 	    cmd_line.str( start_cmd );
 	    cmd_line.seekp(0, ios_base::end );
