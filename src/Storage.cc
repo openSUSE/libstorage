c8db9352734abc215b7a17600958bee6cb952c67
Arvin Schnell <aschnell@suse.de>
2007-03-22 13:52:37 +0000

    - fixed typo

--- src/Storage.cc
+++ src/Storage.cc
@@ -2182,7 +2182,7 @@ Storage::setCryptPassword( const string& device, const string& pwd )
     {
     int ret = 0;
     assertInit();
-    y2milestone( "device:%s l:%u", device.c_str(), pwd.length() );
+    y2milestone( "device:%s l:%zu", device.c_str(), pwd.length() );
 #ifdef DEBUG_LOOP_CRYPT_PASSWORD
     y2milestone( "password:%s", pwd.c_str() );
 #endif
