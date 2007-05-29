84b027c2763873302cd0968616382586d74e2902
Arvin Schnell <aschnell@suse.de>
2007-05-29 14:18:21 +0000

    - fixed crash during detection (caused by empty key-value-pair) (bug #278169)

--- src/AppUtil.cc
+++ src/AppUtil.cc
@@ -347,7 +347,9 @@ makeMap( const list<string>& l, const string& delim, const string& removeSur )
 	if( (pos=i->find_first_of( delim ))!=string::npos )
 	    {
 	    k = i->substr( 0, pos );
-	    v = i->substr( i->find_first_not_of( delim, pos+1 ) );
+	    string::size_type pos2 = i->find_first_not_of( delim, pos+1 );
+	    if( pos2 != string::npos )
+		v = i->substr( pos2 );
 	    }
 	if( !removeSur.empty() )
 	    {
@@ -360,7 +362,8 @@ makeMap( const list<string>& l, const string& delim, const string& removeSur )
 	    if( !v.empty() && (pos=v.find_last_of(removeSur))==v.size()-1 )
 		v.erase( v.find_last_not_of(removeSur)+1 );
 	    }
-	ret[k] = v;
+	if( !k.empty() && !v.empty() )
+	    ret[k] = v;
 	}
     return( ret );
     }
