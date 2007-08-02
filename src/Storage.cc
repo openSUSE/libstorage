681161ceb4ff401d0ea10677842e6e27acea45e4
Thomas Fehr <fehr@suse.de>
2007-08-02 14:35:29 +0000

    allow mount-by-uuid for swap devices

--- src/Storage.cc
+++ src/Storage.cc
@@ -4793,7 +4793,7 @@ Storage::getFsCapabilities (FsType fstype, FsCapabilities& fscapabilities) const
     static FsCapabilitiesX fatCaps (true, false, true, false, false, false,
 				    false, 0, 16);
 
-    static FsCapabilitiesX swapCaps (true, false, true, false, false, true,
+    static FsCapabilitiesX swapCaps (true, false, true, false, true, true,
 				     true, 16, 64);
 
     static FsCapabilitiesX jfsCaps (false, false, false, false, true, true,
