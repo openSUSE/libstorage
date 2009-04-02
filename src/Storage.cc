b356cd21c08faf2b4357bb3d261bcb656717f92d
Arvin Schnell <aschnell@suse.de>
2009-04-02 12:22:25 +0000

    - fixed setting label for used swap partitions (bnc #490817)

--- src/Storage.cc
+++ src/Storage.cc
@@ -4467,7 +4467,7 @@ Storage::getFsCapabilities (FsType fstype, FsCapabilities& fscapabilities) const
 				    false, 0, 16);
 
     static FsCapabilitiesX swapCaps (true, false, true, false, true, true,
-				     true, 16, 64);
+				     false, 16, 64);
 
     static FsCapabilitiesX jfsCaps (false, false, false, false, true, true,
 				    false, 16, 16*1024);
