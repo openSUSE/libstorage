4ecc465a3fe2ade88bc593548279af3f0ce8a216
Arvin Schnell <aschnell@suse.de>
2009-11-12 12:17:19 +0100

    - avoid local variable

--- storage/DmraidCo.cc
+++ storage/DmraidCo.cc
@@ -216,20 +216,19 @@ string DmraidCo::removeText( bool doing ) const
 string DmraidCo::setDiskLabelText( bool doing ) const
     {
     string txt;
-    string d = nm;
     if( doing )
         {
         // displayed text during action, %1$s is replaced by raid name (e.g. pdc_igeeeadj),
 	// %2$s is replaced by label name (e.g. msdos)
         txt = sformat( _("Setting disk label of raid %1$s to %2$s"),
-		       d.c_str(), labelName().c_str() );
+		       name().c_str(), labelName().c_str());
         }
     else
         {
         // displayed text before action, %1$s is replaced by raid name (e.g. pdc_igeeeadj),
 	// %2$s is replaced by label name (e.g. msdos)
         txt = sformat( _("Set disk label of raid %1$s to %2$s"),
-		      d.c_str(), labelName().c_str() );
+		       name().c_str(), labelName().c_str());
         }
     return( txt );
     }
