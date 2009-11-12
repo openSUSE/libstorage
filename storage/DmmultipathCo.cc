e00ad1e99f12825b3ea6026ac457fa9778ec7d43
Arvin Schnell <aschnell@suse.de>
2009-11-12 14:57:00 +0100

    - removed local variable

--- storage/DmmultipathCo.cc
+++ storage/DmmultipathCo.cc
@@ -255,20 +255,19 @@ DmmultipathCo::getMultipaths()
 string DmmultipathCo::setDiskLabelText( bool doing ) const
     {
     string txt;
-    string d = nm;
     if( doing )
         {
         // displayed text during action, %1$s is replaced by multipath name (e.g. 3600508b400105f590000900000300000),
 	// %2$s is replaced by label name (e.g. msdos)
         txt = sformat( _("Setting disk label of multipath disk %1$s to %2$s"),
-		       d.c_str(), labelName().c_str() );
+		       name().c_str(), labelName().c_str());
         }
     else
         {
         // displayed text before action, %1$s is replaced by multipath name (e.g. 3600508b400105f590000900000300000),
 	// %2$s is replaced by label name (e.g. msdos)
         txt = sformat( _("Set disk label of multipath disk %1$s to %2$s"),
-		      d.c_str(), labelName().c_str() );
+		       name().c_str(), labelName().c_str());
         }
     return( txt );
     }
