51c07ab7eec7bc256d3c96198d97a7fd7dab0539
Arvin Schnell <aschnell@suse.de>
2009-11-25 16:12:27 +0100

    - extended test and base it on system "thalassa"

--- testsuite/backupstate1.cc
+++ testsuite/backupstate1.cc
@@ -1,5 +1,4 @@
 
-#include <stdlib.h>
 #include <iostream>
 
 #include "common.h"
@@ -12,27 +11,29 @@ main()
 {
     cout.setf(std::ios::boolalpha);
 
-    system("mkdir -p tmp");
-    system("rm -rf tmp/*");
-
-    system("cp data/disk_sda tmp");
+    setup_system("thalassa");
 
     StorageInterface* s = createStorageInterface(TestEnvironment());
 
     s->createBackupState("test");
-
-    cout << s->checkBackupState("test") << endl;
-    cout << s->equalBackupStates("test", "", true) << endl;
+    cout << s->checkBackupState("test") << " "
+	 << s->equalBackupStates("test", "", true) << endl;
 
     s->changeFormatVolume("/dev/sda1", true, EXT3);
-
-    cout << s->checkBackupState("test") << endl;
-    cout << s->equalBackupStates("test", "", true) << endl;
+    cout << s->checkBackupState("test") << " "
+	 << s->equalBackupStates("test", "", true) << endl;
 
     s->changeFormatVolume("/dev/sda1", false, EXT3);
+    cout << s->checkBackupState("test") << " "
+	 << s->equalBackupStates("test", "", true) << endl;
 
-    cout << s->checkBackupState("test") << endl;
-    cout << s->equalBackupStates("test", "", true) << endl;
+    s->changeMountBy("/dev/system/abuild", MOUNTBY_DEVICE);
+    cout << s->checkBackupState("test") << " "
+	 << s->equalBackupStates("test", "", true) << endl;
+
+    s->changeMountBy("/dev/system/abuild", MOUNTBY_UUID);
+    cout << s->checkBackupState("test") << " "
+	 << s->equalBackupStates("test", "", true) << endl;
 
     delete s;
 }
