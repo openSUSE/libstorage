46c9e477f73dc0630279b6d9435658b924ac7336
Arvin Schnell <aschnell@suse.de>
2009-12-02 11:20:03 +0100

    - removed call to devmap_mknod.sh (which has disappeared)

--- storage/Dm.cc
+++ storage/Dm.cc
@@ -483,7 +483,6 @@ void Dm::activate( bool val )
 		c.execute(GREPBIN " \"^dm[-_]snapshot[ \t]\" /proc/modules");
 		if( c.numLines()<=0 )
 		    c.execute(MODPROBEBIN " dm-snapshot");
-		c.execute("/sbin/devmap_mknod.sh");
 		}
 	    }
 	else
