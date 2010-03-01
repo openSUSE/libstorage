f44aa1bc549d15c0beb53c5eeeae6e2121e0b33e
Arvin Schnell <aschnell@suse.de>
2010-03-01 13:52:06 +0100

    - base testcase on system thalassa

--- testsuite/md_create.cc
+++ testsuite/md_create.cc
@@ -52,45 +52,41 @@ main()
 {
     setup_logger();
 
-    setup_system();
+    setup_system("thalassa");
     
     deque<string> devs;
-
-    system ("cp data/disk_hda.info tmp");
-    system ("cp data/disk_hdb.info tmp");
-    system ("cp data/disk_sda.info tmp");
 
     /*
      * Check that we _cannot_ create software raid devices with just one
      * partition
      */
-    devs.push_back( "/dev/hda1" );
+    devs.push_back("/dev/sdc1");
     cout << "Devices: " << devs.size() << " ----------------------\n";
     createMD(RAID0, devs); // fails, too few devices
     createMD(RAID1, devs); // fails, too few devices
     createMD(RAID5, devs); // fails, too few devices
     createMD(RAID6, devs); // fails, too few devices
-    createMD(RAID10,devs); // fails, too few devices
+    createMD(RAID10, devs); // fails, too few devices
     createMD(MULTIPATH, devs); // fails, too few devices
     createMD(RAID_UNK, devs); // RAID_UNK always fails
 
     /*
      * Check that this works for some raid levels with two devices
      */
-    devs.push_back( "/dev/hda2" );
+    devs.push_back("/dev/sdc2");
     cout << "Devices: " << devs.size() << " ----------------------\n";
     createMD(RAID0, devs); // works now
     createMD(RAID1, devs); // works now
     createMD(RAID5, devs); // fails, too few devices
     createMD(RAID6, devs); // fails, too few devices
-    createMD(RAID10,devs); // works now
+    createMD(RAID10, devs); // works now
     createMD(MULTIPATH, devs); // works now
     createMD(RAID_UNK, devs); // RAID_UNK always fails
 
     /*
      * RAID6 still fails
      */
-    devs.push_back( "/dev/hda3" );
+    devs.push_back("/dev/sdc3");
     cout << "Devices: " << devs.size() << " ----------------------\n";
     createMD(RAID5, devs); // works now
     createMD(RAID6, devs); // fails, too few devices
@@ -98,7 +94,7 @@ main()
     /* 
      * Everything works now
      */
-    devs.push_back( "/dev/hda3" );
+    devs.push_back("/dev/sdc5");
     cout << "Devices: " << devs.size() << " ----------------------\n";
     createMD(RAID6, devs); // works now
 
@@ -106,25 +102,12 @@ main()
      * Check that we can create a software raid device with partitions
      * from different devices
      */
-    devs.push_back( "/dev/hdb1" );
+    devs.push_back("/dev/sdb1");
     cout << "Devices: " << devs.size() << " ----------------------\n";
     createMD(RAID0, devs);
     createMD(RAID1, devs);
     createMD(RAID5, devs);
     createMD(RAID6, devs);
-    createMD(RAID10,devs);
-    createMD(MULTIPATH, devs);
-
-
-    /*
-     * Check that we can create a software raid device with partitions from a scsi disk
-     */
-    devs.push_back( "/dev/sda1" );
-    cout << "Devices: " << devs.size() << " ----------------------\n";
-    createMD(RAID0, devs);
-    createMD(RAID1, devs);
-    createMD(RAID5, devs);
-    createMD(RAID6, devs);
-    createMD(RAID10,devs);
+    createMD(RAID10, devs);
     createMD(MULTIPATH, devs);
 }
