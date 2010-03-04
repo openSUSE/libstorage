b0a2bfd2a66fa7b42692dc149767c740e8cc481c
Arvin Schnell <aschnell@suse.de>
2010-03-04 17:58:25 +0100

    - better readable output

--- testsuite/humanstring1.cc
+++ testsuite/humanstring1.cc
@@ -26,15 +26,19 @@ main()
     if (localedir)
 	bindtextdomain("libstorage", localedir);
 
+    cout << "-- 1 --" << endl;
     test("en_GB.UTF-8", 1024, true, 2, true);
     test("en_GB.UTF-8", 1024, true, 2, false);
 
+    cout << "-- 2 --" << endl;
     test("de_DE.UTF-8", 123456789, true, 4, false);
     test("de_DE.UTF-8", 123456789, false, 4, false);
 
+    cout << "-- 3 --" << endl;
     test("fr_FR.UTF-8", 123456789, true, 4, false);
     test("fr_FR.UTF-8", 123456789, false, 4, false);
 
+    cout << "-- 4 --" << endl;
     test("en_GB.UTF-8", 1000*1024, false, 2, false);
     test("de_DE.UTF-8", 1000*1024, false, 2, false);
     test("fr_FR.UTF-8", 1000*1024, false, 2, false);
--- testsuite/humanstring2.cc
+++ testsuite/humanstring2.cc
@@ -31,41 +31,51 @@ main()
     if (localedir)
 	bindtextdomain("libstorage", localedir);
 
+    cout << "-- 1 --" << endl;
     test("en_GB.UTF-8", "42", true);			// FAILS: classic=true needs a suffix
     test("en_GB.UTF-8", "42B", true);
     test("en_GB.UTF-8", "42 b", true);
 
+    cout << "-- 2 --" << endl;
     test("en_GB.UTF-8", "42", false);
     test("en_GB.UTF-8", "42b", false);
     test("en_GB.UTF-8", "42 B", false);
 
+    cout << "-- 3 --" << endl;
     test("en_GB.UTF-8", "12.4GB", true);
     test("en_GB.UTF-8", "12.4 GB", true);
 
+    cout << "-- 4 --" << endl;
     test("en_GB.UTF-8", "12.4GB", false);
     test("en_GB.UTF-8", "12.4 gb", false);
     test("en_GB.UTF-8", "12.4g", false);
     test("en_GB.UTF-8", "12.4 G", false);
 
+    cout << "-- 5 --" << endl;
     test("en_GB.UTF-8", "123,456 kB", false);
     test("de_DE.UTF-8", "123.456 kB", false);
     test("fr_FR.UTF-8", "123 456 ko", false);
 
+    cout << "-- 6 --" << endl;
     test("en_GB.UTF-8", "123,456.789kB", false);
     test("de_DE.UTF-8", "123.456,789kB", false);
     test("fr_FR.UTF-8", "123 456,789ko", false);
 
+    cout << "-- 7 --" << endl;
     test("en_GB.UTF-8", "123,456.789 kB", false);
     test("de_DE.UTF-8", "123.456,789 kB", false);
     test("fr_FR.UTF-8", "123 456,789 ko", false);
 
+    cout << "-- 8 --" << endl;
     test("fr_FR.UTF-8", "5Go", false);
     test("fr_FR.UTF-8", "5 Go", false);
 
+    cout << "-- 9 --" << endl;
     test("en_US.UTF-8", "5 G B", false);		// FAILS
     test("de_DE.UTF-8", "12.34 kB", false);		// FAILS
     test("fr_FR.UTF-8", "12 34 Go", false);		// FAILS
 
+    cout << "-- 10 --" << endl;
     test("en_GB.UTF-8", "3.14 G", false);
     test("en_GB.UTF-8", "3.14 GB", false);
     test("en_GB.UTF-8", "3.14 GiB", false);
