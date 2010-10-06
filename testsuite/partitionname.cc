471185701b5711154dd35923b0bbb32f9862155e
Arvin Schnell <aschnell@suse.de>
2010-10-06 17:46:27 +0200

    - extended testcase

--- testsuite/partitionname.cc
+++ testsuite/partitionname.cc
@@ -19,6 +19,7 @@ main()
     cout << s->getPartitionName("/dev/cciss/c0d1", 2) << endl;
     cout << s->getPartitionName("/dev/mmcblk0", 3) << endl;
     cout << s->getPartitionName("/dev/mapper/isw_cfcjajfdfh_test", 4) << endl;
+    cout << s->getPartitionName("/dev/md2", 5) << endl;
 
     delete s;
 }
