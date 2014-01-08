67465c27dc1b5c84119323e144a1b4be52c40448
Arvin Schnell <aschnell@suse.de>
2014-01-08 18:17:11 +0100

    - extended unit-tests

--- testsuite/partitionname.cc
+++ testsuite/partitionname.cc
@@ -10,16 +10,24 @@ using namespace storage;
 
 int
 main()
-{ 
+{
     setup_logger();
 
     StorageInterface* s = createStorageInterface(TestEnvironment());
 
     cout << s->getPartitionName("/dev/sda", 1) << endl;
+
     cout << s->getPartitionName("/dev/cciss/c0d1", 2) << endl;
+
     cout << s->getPartitionName("/dev/mmcblk0", 3) << endl;
+
     cout << s->getPartitionName("/dev/mapper/isw_cfcjajfdfh_test", 4) << endl;
+
     cout << s->getPartitionName("/dev/md2", 5) << endl;
+
+    cout << s->getPartitionName("/dev/rsxx0", 6) << endl;
+
+    cout << s->getPartitionName("/dev/nvme0n1", 7) << endl;
 
     delete s;
 }
--- testsuite/partitionprefix.cc
+++ testsuite/partitionprefix.cc
@@ -10,19 +10,28 @@ using namespace storage;
 
 int
 main()
-{ 
+{
     setup_logger();
 
     StorageInterface* s = createStorageInterface(TestEnvironment());
 
     cout << s->getPartitionPrefix("/dev/sda") << endl;
     cout << s->getPartitionPrefix("/dev/sda1") << endl;
+
     cout << s->getPartitionPrefix("/dev/cciss/c0d1") << endl;
     cout << s->getPartitionPrefix("/dev/cciss/c0d1p2") << endl;
+
     cout << s->getPartitionPrefix("/dev/mmcblk0") << endl;
     cout << s->getPartitionPrefix("/dev/mmcblk0p3") << endl;
+
     cout << s->getPartitionPrefix("/dev/mapper/isw_cfcjajfdfh_test") << endl;
     cout << s->getPartitionPrefix("/dev/mapper/isw_cfcjajfdfh_test-part4") << endl;
+
+    cout << s->getPartitionPrefix("/dev/rsxx0") << endl;
+    cout << s->getPartitionPrefix("/dev/rsxx0p5") << endl;
+
+    cout << s->getPartitionPrefix("/dev/nvme0n1") << endl;
+    cout << s->getPartitionPrefix("/dev/nvme0n1p6") << endl;
 
     delete s;
 }
