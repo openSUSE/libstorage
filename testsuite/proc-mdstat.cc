29d4e6cbeb14a75a8670677903337ae797356143
Arvin Schnell <aschnell@suse.de>
2014-01-15 10:25:27 +0100

    - added testcases

--- testsuite/proc-mdstat.cc
+++ testsuite/proc-mdstat.cc
@@ -32,6 +32,58 @@ parse1()
 }
 
 
+void
+parse2()
+{
+    cout << "parse2" << endl;
+
+    vector<string> lines = {
+	"Personalities : [raid1] [raid0] ",
+	"md125 : active (auto-read-only) raid1 sda[1] sdb[0]",
+	"      4194304 blocks super external:/md127/0 [2/2] [UU]",
+	"      ",
+	"md126 : active raid0 sda[1] sdb[0]",
+	"      8378790 blocks super external:/md127/1 128k chunks",
+	"      ",
+	"md127 : inactive sdb[1](S) sda[0](S)",
+	"      5104 blocks super external:imsm",
+	"       ",
+	"unused devices: <none>",
+    };
+
+    ProcMdstat procmdstat(false);
+    procmdstat.parse(lines);
+
+    cout << procmdstat << endl;
+}
+
+
+void
+parse3()
+{
+    cout << "parse3" << endl;
+
+    vector<string> lines = {
+	"Personalities : [raid1] [raid0] ",
+	"md125 : active raid1 sda[1] sdb[0]",
+	"      7355904 blocks super external:/md127/1 [2/2] [UU]",
+	"      ",
+	"md126 : active raid0 sda[1] sdb[0]",
+	"      1999872 blocks super external:/md127/0 512k chunks",
+	"      ",
+	"md127 : inactive sdb[1](S) sda[0](S)",
+	"      65536 blocks super external:ddf",
+	"       ",
+	"unused devices: <none>",
+    };
+
+    ProcMdstat procmdstat(false);
+    procmdstat.parse(lines);
+
+    cout << procmdstat << endl;
+}
+
+
 int
 main()
 {
@@ -40,4 +92,6 @@ main()
     setup_logger();
 
     parse1();
+    parse2();
+    parse3();
 }
