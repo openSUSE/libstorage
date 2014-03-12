f858b637d520f5c1c253d0e1e3b3880dd307108c
Arvin Schnell <aschnell@suse.de>
2014-03-12 09:51:38 +0100

    - split SystemInfo example

---
+++ examples/SystemInfo/test-blkid.cc
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+#include <storage/SystemInfo/SystemInfo.h>
+
+using namespace std;
+using namespace storage;
+
+
+void
+test_blkid(SystemInfo& systeminfo)
+{
+    try
+    {
+	const Blkid& blkid = systeminfo.getBlkid();
+	cout << "Blkid success" << endl;
+	cout << blkid << endl;
+    }
+    catch (const exception& e)
+    {
+	cerr << "Blkid failed" << endl;
+    }
+}
+
+
+int
+main()
+{
+    createLogger("/var/log/YaST2", "libstorage");
+
+    SystemInfo systeminfo;
+
+    test_blkid(systeminfo);
+    test_blkid(systeminfo);
+}
---
+++ examples/SystemInfo/test-parted.cc
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+#include <storage/SystemInfo/SystemInfo.h>
+
+using namespace std;
+using namespace storage;
+
+
+void
+test_parted(SystemInfo& systeminfo, const string& device)
+{
+    try
+    {
+	const Parted& parted = systeminfo.getParted(device);
+	cout << "Parted success for '" << device << "'" << endl;
+	cout << parted << endl;
+    }
+    catch (const exception& e)
+    {
+	cerr << "Parted failed for '" << device << "'" << endl;
+    }
+}
+
+
+int
+main()
+{
+    createLogger("/var/log/YaST2", "libstorage");
+
+    SystemInfo systeminfo;
+
+    test_parted(systeminfo, "/dev/sda");
+    test_parted(systeminfo, "/dev/sda");
+}
