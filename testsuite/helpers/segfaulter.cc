58114b3362df4eec605a0c8ae024d362f52eb3f1
Ladislav Slezák <lslezak@suse.cz>
2018-02-28 13:56:08 +0100

    Drop the old files

--- testsuite/helpers/segfaulter.cc
+++
@@ -1,8 +0,0 @@
-// Helper program for testing the SystemCmd class:
-// This program intentionally causes a segfault.
-
-int main( int argc, char *argv[] )
-{
-    char *null_pointer = 0;
-    *null_pointer = 'a';
-}
